9155a94a43ec255777a4591e69e10fc30e76a857
Alwi <alwirifa7@gmail.com>
2024-06-14 17:46:08 +0700

    5th commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,7 @@
-import Container from "@/components/home/Container";
+
 import TentangKemenkes from "@/components/home/TentangKemenkes";
 import Welcome from "@/components/home/Welcome";
 import BeritaTerkini from "@/components/home/Berita";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
