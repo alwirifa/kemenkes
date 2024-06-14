24d5b226686b6da9b6e9de8f6c1d32d88792b696
Alwi <alwirifa7@gmail.com>
2024-06-14 17:49:29 +0700

    6th

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,12 @@
 
 import TentangKemenkes from "@/components/home/TentangKemenkes";
 import Welcome from "@/components/home/Welcome";
-import BeritaTerkini from "@/components/home/Berita";
 
 export default function Home() {
   return (
     <div>
       <Welcome />
       <TentangKemenkes/>
-      <BeritaTerkini />
     </div>
   );
 }
